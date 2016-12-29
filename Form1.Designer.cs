e92074c167f85a29d221bbbc9d15ca13ff837e6a
david-olivan <david.olivan.93@gmail.com>
2016-12-29 18:43:57 +0100

    First Update
    The backbone of the program

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DialogueEditor
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -25,12 +25,231 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent () {
-            this.components = new System.ComponentModel.Container ();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zenoreGameStudiosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dialogueNodeBox = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.dialogueName = new DialogueEditor.WaterMarkTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(780, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem1,
+            this.quitToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.newToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem1
+            // 
+            this.newToolStripMenuItem1.Name = "newToolStripMenuItem1";
+            this.newToolStripMenuItem1.Size = new System.Drawing.Size(98, 22);
+            this.newToolStripMenuItem1.Text = "New";
+            this.newToolStripMenuItem1.Click += new System.EventHandler(this.newToolStripMenuItem1_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zenoreGameStudiosToolStripMenuItem,
+            this.programToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // zenoreGameStudiosToolStripMenuItem
+            // 
+            this.zenoreGameStudiosToolStripMenuItem.Name = "zenoreGameStudiosToolStripMenuItem";
+            this.zenoreGameStudiosToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.zenoreGameStudiosToolStripMenuItem.Text = "ZenoreGameStudios";
+            this.zenoreGameStudiosToolStripMenuItem.Click += new System.EventHandler(this.zenoreGameStudiosToolStripMenuItem_Click);
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.programToolStripMenuItem.Text = "Program";
+            this.programToolStripMenuItem.Click += new System.EventHandler(this.programToolStripMenuItem_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.Controls.Add(this.dialogueNodeBox);
+            this.mainPanel.Controls.Add(this.groupBox2);
+            this.mainPanel.Controls.Add(this.groupBox1);
+            this.mainPanel.Location = new System.Drawing.Point(0, 30);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(780, 526);
+            this.mainPanel.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dialogueName);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 94);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Main Information";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(52, 54);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(129, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nodes:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 103);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 277);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controls";
+            // 
+            // dialogueNodeBox
+            // 
+            this.dialogueNodeBox.Location = new System.Drawing.Point(218, 3);
+            this.dialogueNodeBox.Name = "dialogueNodeBox";
+            this.dialogueNodeBox.Size = new System.Drawing.Size(550, 377);
+            this.dialogueNodeBox.TabIndex = 2;
+            this.dialogueNodeBox.TabStop = false;
+            this.dialogueNodeBox.Text = "Dialogue Node";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnNew);
+            this.panel1.Location = new System.Drawing.Point(250, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 291);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(52, 150);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(150, 50);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // dialogueName
+            // 
+            this.dialogueName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.dialogueName.Location = new System.Drawing.Point(52, 20);
+            this.dialogueName.Name = "dialogueName";
+            this.dialogueName.Size = new System.Drawing.Size(129, 20);
+            this.dialogueName.TabIndex = 1;
+            this.dialogueName.WaterMarkColor = System.Drawing.Color.Gray;
+            this.dialogueName.WaterMarkText = "Introduce a name";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(780, 558);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Dialogue Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.mainPanel.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zenoreGameStudiosToolStripMenuItem;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private WaterMarkTextBox dialogueName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox dialogueNodeBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNew;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,18 +2,74 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace DialogueEditor
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1 () {
+        private XmlDocument xmlDocument;
+
+        private Panel theMainPanel;
+        private WaterMarkTextBox txtDialogueName;
+        private GroupBox grpbDialogueNodeBox;
+        private bool isDialogueOpen;
+
+        public Main () {
             InitializeComponent ();
+
+            InitializeEverything ();
+        }
+
+        private void InitializeEverything () {
+            isDialogueOpen = false;
+            theMainPanel = mainPanel;
+            theMainPanel.Enabled = false;
+            theMainPanel.Visible = false;
+            txtDialogueName = dialogueName;
+            grpbDialogueNodeBox = dialogueNodeBox;
+            grpbDialogueNodeBox.Visible = false;
+            grpbDialogueNodeBox.Enabled = false;
+        }
+
+        private void CreateNew () {
+            isDialogueOpen = true;
+        }
+        private void InitializeGUI () {
+            theMainPanel.Enabled = true;
+        }
+
+        //#region Button functions
+        private void newToolStripMenuItem1_Click (object sender, EventArgs e) {
+            if (isDialogueOpen) {
+                var confirmResult = MessageBox.Show ("A dialogue is still open, are you sure you want to open a new one?",
+                    "Confirm delete current",
+                    MessageBoxButtons.OKCancel);
+                if (confirmResult == DialogResult.OK) {
+                    CreateNew ();
+                    InitializeGUI ();
+                }
+            } else {
+                CreateNew ();
+                InitializeGUI ();
+            }
+            
+        }
+        private void quitToolStripMenuItem_Click (object sender, EventArgs e) {
+            Application.Exit ();
+        }
+        private void zenoreGameStudiosToolStripMenuItem_Click (object sender, EventArgs e) {
+            Process.Start ("http://zenoregamestudios.azurewebsites.net/");
+        }
+        private void programToolStripMenuItem_Click (object sender, EventArgs e) {
+            MessageBox.Show ("This program was created by the team at ZenoreGameStudios to be able " +
+                "to easily create dialogues for our Unity Dialogue Module");
         }
     }
 }
